0830d42f52019dc609d8fc1dfb0e4cdcdd27c3b4
heazum <hizhnyakk@gmail.com>
2018-07-12 12:58:38 +0300

    Update SQL with Harry Potter: Sorting Hat Comparators.py

--- SQL/SQL with Harry Potter: Sorting Hat Comparators.py
+++ SQL/SQL with Harry Potter: Sorting Hat Comparators.py
@@ -1,4 +1,4 @@
-There is truly no magic in the world; the Hogwarts Sorting Hat is SQL-based, its decision-making powers are common operators and prospectIve students are merely data - names, and two columns of qualities.
+/*There is truly no magic in the world; the Hogwarts Sorting Hat is SQL-based, its decision-making powers are common operators and prospectIve students are merely data - names, and two columns of qualities.
 
 students
 
@@ -13,7 +13,7 @@ Hufflepuff will simply take those who have the quality hufflepuff.
 
 (don't worry, for simplicity's sake 'brave' and 'studious' will only appear in quality1, and 'cunning' and 'intelligent' will only appear in quality2.)
 
-Return the id, name, quality1 and quality2 of all the students who'll be accepted, ordered by ascending id.
+Return the id, name, quality1 and quality2 of all the students who'll be accepted, ordered by ascending id.*/
 
 
 
